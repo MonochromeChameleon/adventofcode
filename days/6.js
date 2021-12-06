7e150ca5d36aa8bd34ab26d927111cf7294f4aa3
Hugh Greenish <hugh.greenish@mmc.com>
2021-12-06 22:26:57 +0000

    I like this better now

--- days/6.js
+++ days/6.js
@@ -5,37 +5,51 @@ export class Question extends QuestionBase {
     super(6, 5934, 26984457539, 380612, 1710166656900, args);
   }
 
-  parseLine(line) {
+  parseLine (line) {
     return line.split(',').map(Number);
   }
 
-  parseInput(lines) {
+  parseInput (lines) {
     return lines.flatMap(this.parseLine);
   }
 
-  initializeFish(days) {
-    const reproRate = 7;
-    const initialRate = 9;
-
-    const countNewFish = (startDay, fishCounts) => {
-      const fishDays = Array.from({ length: ~~(days / reproRate) + 1 }).map((_, ix) => startDay + initialRate + (ix * reproRate)).filter(it => it <= days);
-      return 1 + fishDays.reduce((acc, day) => acc + fishCounts[day], 0);
-    }
-
-    return Array.from({ length: days + initialRate }).reduce((sofar, _, ix) => {
-      const offsetDays = days - ix;
-      sofar[offsetDays] = countNewFish(offsetDays, sofar);
+  countFish (input, days) {
+    const startingCount = input.reduce((sofar, fish) => {
+      sofar[fish] = (sofar[fish] || 0) + 1;
       return sofar;
     }, {});
+    const endCount = Array.from({ length: days }).reduce(({
+      0: zero = 0,
+      1: one = 0,
+      2: two = 0,
+      3: three = 0,
+      4: four = 0,
+      5: five = 0,
+      6: six = 0,
+      7: seven = 0,
+      8: eight = 0,
+    }) => {
+      return {
+        0: one,
+        1: two,
+        2: three,
+        3: four,
+        4: five,
+        5: six,
+        6: seven + zero,
+        7: eight,
+        8: zero,
+      };
+    }, startingCount);
+
+    return Object.values(endCount).reduce((sofar, count) => sofar + count, 0);
   }
 
   part1 (input) {
-    const fishies = this.initializeFish(80);
-    return input.map(f => fishies[f - 8]).reduce((sofar, f) => sofar + f, 0);
+    return this.countFish(input, 80);
   }
 
   part2 (input) {
-    const fishies = this.initializeFish(256);
-    return input.map(f => fishies[f - 8]).reduce((sofar, f) => sofar + f, 0);
+    return this.countFish(input, 256);
   }
 }
