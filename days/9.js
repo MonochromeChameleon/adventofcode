81eac4f04aad7d61364ab4c3238fcce96e78baac
Hugh Greenish <hugh.greenish@mmc.com>
2021-12-09 23:09:59 +0000

    tidier

--- days/9.js
+++ days/9.js
@@ -6,26 +6,21 @@ export class Question extends QuestionBase {
   }
 
   parseLine(line) {
-    return line.split('').map(Number);
+    // Pad the start and end with a 9 so we have proper bounds
+    return [9, ...line.split('').map(Number), 9];
   }
 
   parseInput (lines) {
-    const grid = lines.flatMap(this.parseLine);
-    const width = grid.length / lines.length;
-    const height = lines.length;
+    const innerGrid = [...lines.flatMap(this.parseLine)];
+    const width = innerGrid.length / lines.length;
+    const nines = Array.from({ length: width }, () => 9);
+    const grid = [...nines, ...innerGrid, ...nines];
 
-    return { grid, width, height };
+    return { grid, width };
   }
 
   part1 ({ grid, width }) {
-    const isMinimum = (value, ix) => {
-      const up = ix - width >= 0 ? grid[ix - width] : 9;
-      const down = ix + width < grid.length ? grid[ix + width] : 9;
-      const left = ix % width === 0 ? 9 : grid[ix - 1];
-      const right = (ix + 1) % width === 0 ? 9 : grid[ix + 1];
-
-      return [up, down, left, right].every(it => it > value);
-    }
+    const isMinimum = (value, ix) => [width, -width, 1, -1].every(d => grid[ix + d] === undefined || grid[ix + d] > value);
     const minima = grid.filter(isMinimum);
     return minima.reduce((sum, point) => sum + point, 0) + minima.length;
   }
@@ -36,29 +31,29 @@ export class Question extends QuestionBase {
     const { basins } = grid.reduce((state, point, ix) => {
       if (point === 9) return state;
 
-      const left = ix % width === 0 ? undefined : ix - 1;
-      const up = ix - width >= 0 ? ix - width : undefined;
+      const left = ix - 1;
+      const up = ix - width;
 
-      const isNew = [left, up].every(it => grid[it] === undefined || grid[it] === 9);
+      const isNew = [left, up].every(it => grid[it] === 9);
+
+      const leftBasin = findRootBasin(state.mapped[left], state.combined);
+      const upBasin = findRootBasin(state.mapped[up], state.combined);
+
+      const [basin = state.basins.length, combine = basin] = [leftBasin, upBasin].filter(it => it !== undefined).sort((a, b) => a - b);
       if (isNew) {
-        const basin = state.basins.length;
-        state.mapped[ix] = basin;
+        state.basins.push(0);
         state.combined[basin] = basin;
-        state.basins.push(1);
-      } else {
-        const leftBasin = findRootBasin(state.mapped[left], state.combined);
-        const upBasin = findRootBasin(state.mapped[up], state.combined);
-
-        const [basin, combine] = [leftBasin, upBasin].filter(it => it !== undefined).sort((a, b) => a - b);
-        state.mapped[ix] = basin;
-        state.basins[basin] += 1;
-
-        if (combine && combine !== basin) {
-          state.basins[basin] += state.basins[combine];
-          state.basins[combine] = 0;
-          state.combined[combine] = state.combined[basin];
-        }
       }
+
+      state.mapped[ix] = basin;
+      state.basins[basin] += 1;
+
+      if (combine !== basin) {
+        state.basins[basin] += state.basins[combine];
+        state.basins[combine] = 0;
+        state.combined[combine] = state.combined[basin];
+      }
+
       return state;
     }, { basins: [0], mapped: [], combined: {} });
 
