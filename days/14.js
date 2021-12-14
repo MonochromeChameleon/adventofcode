080d244316b630a93faaaed49ef459fa5ad3372f
Hugh Greenish <hugh.greenish@mmc.com>
2021-12-14 09:08:43 +0000

    day 14

---
+++ days/14.js
@@ -0,0 +1,71 @@
+import { QuestionBase } from '../utils/question-base.js';
+import { countByValue } from '../utils/count-by-value.js';
+
+export class Question extends QuestionBase {
+  constructor (args) {
+    super(14, 1588, 2712, 2188189693529, 8336623059567, args);
+  }
+
+  polymerize(polymer, insertionRules) {
+    return Object.keys(polymer).reduce((out, key) => {
+      const add = insertionRules[key];
+      const [first, second] = key.split('');
+      [first + add, add + second].forEach(newKey => {
+        out[newKey] = out[newKey] || 0;
+        out[newKey] += polymer[key];
+      })
+
+      return out;
+    }, { });
+  }
+
+  runPolymerizationSteps(polymer, insertionRules, steps) {
+    return Array.from({ length: steps }).reduce((poly) => this.polymerize(poly, insertionRules), polymer);
+  }
+
+  countElements(polymer) {
+    return Object.keys(polymer).reduce((acc, key) => {
+      const [_, second] = key.split('');
+      acc[second] = acc[second] || 0;
+      acc[second] += polymer[key];
+      return acc;
+    }, { N: 1 });
+  }
+
+  parseLine(line) {
+    return line.split(' -> ');
+  }
+
+  parseInput([template, ...lines]) {
+    const insertionRules = lines.map(this.parseLine).reduce((acc, [key, value]) => {
+      acc[key] = value;
+      return acc;
+    }, {});
+
+    const pairs = Array.from({ length: template.length - 1 }).reduce((acc, _, index) => {
+      const pair = template.slice(index, index + 2);
+      acc[pair] = acc[pair] || 0;
+      acc[pair] += 1;
+      return acc;
+    }, {});
+
+    return {
+      pairs,
+      insertionRules
+    }
+  }
+
+  part1 ({ pairs, insertionRules }) {
+    const polymer = this.runPolymerizationSteps(pairs, insertionRules, 10);
+    const countsByCharacter = this.countElements(polymer);
+    const counts = Object.values(countsByCharacter).sort((a, b) => a - b);
+    return counts[counts.length - 1] - counts[0];
+  }
+
+  part2 ({ pairs, insertionRules }) {
+    const polymer = this.runPolymerizationSteps(pairs, insertionRules, 40);
+    const countsByCharacter = this.countElements(polymer);
+    const counts = Object.values(countsByCharacter).sort((a, b) => a - b);
+    return counts[counts.length - 1] - counts[0];
+  }
+}
