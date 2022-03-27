9600d0d23cc7ee7459f97dee6cd8ca1ba7d4855c
Hugh Greenish <hugh.greenish@mmc.com>
2022-03-27 08:39:08 +0100

    14/15

--- answers/2020/14.js
+++ answers/2020/14.js
@@ -1,8 +1,9 @@
 import { QuestionBase, Parsers } from '../../utils/question-base.js';
+import { allCombinations } from '../../utils/array-utils.js';
 
 export class Question extends QuestionBase {
   constructor() {
-    super(2020, 14, 16003257187056);
+    super(2020, 14, 16003257187056, 3219837697833);
 
     this.exampleInput({ part1: 165 });
     this.exampleInput({ part2: 208 });
@@ -25,12 +26,11 @@ export class Question extends QuestionBase {
       const [, address, value] = a.match(/^mem\[(\d+)] = (\d+)$/).map(Number);
       return { address, value };
     });
-    return { mask: mask.substring(6).split(''), assignments: ass };
+    return { mask: mask.substring(7).split(''), assignments: ass };
   }
 
   part1(input) {
-    const result = input.reduce(
-      (memory, { mask, assignments }) =>
+    const result = input.reduce((memory, { mask, assignments }) =>
         assignments.reduce((mem, { address, value }) => {
           const binaryValue = value.toString(2).padStart(mask.length, '0');
           const maskedValue = mask.map((bit, ix) => (bit === 'X' ? binaryValue[ix] : bit)).join('');
@@ -43,17 +43,30 @@ export class Question extends QuestionBase {
   }
 
   part2(input) {
-    const result = input.reduce(
-      ({ mask, memory }, line) => {
-        if (line.startsWith('mask')) return { mask: line.substring(6).split(''), memory };
-        const [, address, value] = line.match(/^mem\[(\d+)] = (\d+)$/).map(Number);
-        const binaryValue = value.toString(2).padStart(mask.length, '0');
-        const maskedValue = mask.map((bit, ix) => (bit === 'X' ? binaryValue[ix] : bit)).join('');
-        return { mask, memory: { ...memory, [address]: parseInt(maskedValue, 2) } };
-      },
-      { mask: [], memory: {} }
-    );
+    const result = input.reduce((memory, { mask, assignments }) => {
+      const floaters = mask.map((bit, ix) => (bit === 'X' ? (mask.length - ix - 1) : -1)).filter((ix) => ix !== -1);
+      const allFloaters = allCombinations(floaters).map((f) => f.map((p) => 2 ** p).reduce((a, b) => a + b, 0));
 
-    return Object.values(result.memory).reduce((sum, value) => sum + value, 0);
+      return assignments.reduce((mem, { address, value }) => {
+        const binaryAddress = address.toString(2).padStart(mask.length, '0');
+        const maskedAddress = mask.map((bit, ix) => {
+          switch (bit) {
+            case '0':
+              return binaryAddress[ix];
+            case '1':
+              return '1';
+            case 'X':
+              return '0';
+          }
+        }).join('');
+        const baseAddress = parseInt(maskedAddress, 2);
+        allFloaters.map((f) => f + baseAddress).forEach((address) => {
+          mem[address] = value;
+        });
+        return mem;
+      }, memory);
+    }, {});
+
+    return Object.values(result).reduce((sum, value) => sum + value, 0);
   }
 }
--- answers/2020/15.js
+++ answers/2020/15.js
@@ -2,18 +2,44 @@ import { QuestionBase, Parsers } from '../../utils/question-base.js';
 
 export class Question extends QuestionBase {
   constructor() {
-    super(2020, 15);
+    super(2020, 15, 536, 24065124);
+
+    this.exampleInput({ input: '0,3,6', part1: 436, part2: 175594 });
+    this.exampleInput({ input: '1,3,2', part1: 1, part2: 2578 });
+    this.exampleInput({ input: '2,1,3', part1: 10, part2: 3544142 });
+    this.exampleInput({ input: '1,2,3', part1: 27, part2: 261214 });
+    this.exampleInput({ input: '2,3,1', part1: 78, part2: 6895259 });
+    this.exampleInput({ input: '3,2,1', part1: 438, part2: 18 });
+    this.exampleInput({ input: '3,1,2', part1: 1836, part2: 362 });
   }
 
   get parser() {
-    return Parsers.ONE_NUMBER_PER_LINE;
+    return Parsers.SINGLE_LINE_DELIMITED_NUMBERS;
+  }
+
+  get split() {
+    return ',';
+  }
+
+  play(input, turns) {
+    const history = new Array(turns).fill(undefined);
+    input.slice(0, -1).forEach((n, i) => {
+      history[n] = i;
+    });
+    let prev = input[input.length - 1];
+    for (let i = 0; i < turns - input.length; i += 1) {
+      const next = history[prev] !== undefined ? i - 1 + input.length - history[prev] : 0;
+      history[prev] = i + input.length - 1;
+      prev = next;
+    }
+    return prev
   }
 
   part1(input) {
-    return input.length;
+    return this.play(input, 2020);
   }
 
   part2(input) {
-    return input.reduce((a, b) => a + b, 0);
+    return this.play(input, 30000000);
   }
 }
