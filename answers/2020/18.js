de2174257ebbf7320b97d68415f5d8c22c5a5853
Hugh Greenish <hugh.greenish@mmc.com>
2022-03-28 13:09:09 +0100

    19wip

--- answers/2020/18.js
+++ answers/2020/18.js
@@ -1,19 +1,66 @@
-import { QuestionBase, Parsers } from '../../utils/question-base.js';
+import { QuestionBase } from '../../utils/question-base.js';
 
 export class Question extends QuestionBase {
   constructor() {
-    super(2020, 18);
+    super(2020, 18, 464478013511, 85660197232452);
+
+    this.exampleInput({ input: '1 + 2 * 3 + 4 * 5 + 6', part1: 71, part2: 231 });
+    this.exampleInput({ input: '1 + (2 * 3) + (4 * (5 + 6))', part1: 51 });
+    this.exampleInput({ input: '2 * 3 + (4 * 5)', part1: 26, part2: 46 });
+    this.exampleInput({ input: '5 + (8 * 3 + 9 + 3 * 4 * 3)', part1: 437, part2: 1445 });
+    this.exampleInput({ input: '5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))', part1: 12240, part2: 669060 });
+    this.exampleInput({ input: '((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2', part1: 13632, part2: 23340 });
   }
 
-  get parser() {
-    return Parsers.ONE_NUMBER_PER_LINE;
+  parseLine(line) {
+    const parseToken = (token) => {
+      if (token.startsWith('(')) return ['(', ...parseToken(token.slice(1))];
+      if (token.endsWith(')')) return [...parseToken(token.slice(0, -1)), ')'];
+      if (Number.isNaN(Number(token))) return [token];
+      return [Number(token)];
+    };
+
+    return line.split(' ').flatMap(parseToken);
+  }
+
+  evaluate(line) {
+    const result = line.reduce(
+      (state, next) => {
+        if (next === '(') return { value: 0, operation: '+', stack: state };
+        if (['+', '*'].includes(next)) return { ...state, operation: next };
+
+        if (next === ')') {
+          next = state.value;
+          state = state.stack;
+        }
+
+        if (state.operation === '+') {
+          state.value += next;
+        } else {
+          state.value *= next;
+        }
+
+        return state;
+      },
+      { value: 0, operation: '+' }
+    );
+
+    return result.value;
+  }
+
+  fixPrecedence(line) {
+    return ['(', ...line.flatMap((t) => {
+      if (t === '*') return [')', '*', '('];
+      if (t === '(') return ['(', '('];
+      if (t === ')') return [')', ')'];
+      return t;
+    }), ')'];
   }
 
   part1(input) {
-    return input.length;
+    return input.reduce((sum, line) => sum + this.evaluate(line), 0);
   }
-
   part2(input) {
-    return input.reduce((a, b) => a + b, 0);
+    return input.map((line) => this.fixPrecedence(line)).reduce((sum, line) => sum + this.evaluate(line), 0);
   }
 }
--- answers/2020/19.js
+++ answers/2020/19.js
@@ -1,19 +1,86 @@
 import { QuestionBase, Parsers } from '../../utils/question-base.js';
 
+class Rule {
+  constructor(id, rule) {
+    this.id = id;
+    this.rule = rule.replace(/"/g, '');
+    this.rules = rule.split(' | ').map((r) => r.split(' ').map((v) => Number(v)));
+  }
+
+  apply(inputs, ruleLookup) {
+    if (/[ab]/.test(this.rule)) return this.applyLetterMatch(inputs);
+    return this.applyRules(inputs, ruleLookup);
+  }
+
+  applyLetterMatch(inputs) {
+    const matching = inputs.filter((inp) => inp[0] === this.rule);
+    const rest = matching.map((inp) => inp.slice(1)).filter(Boolean);
+    const pass = !!matching.length;
+    return { pass, rest };
+  }
+
+  applyRules(inputs, ruleLookup) {
+    const options = this.rules.map((ruleIds) => ruleIds.map((id) => ruleLookup[id]));
+
+    const results = options.flatMap((rules) => {
+      return inputs.map((input) => {
+        return rules.reduce(({ pass, rest }, rule) => {
+          if (!pass || !rest.length) return { pass: false, rest: [] };
+          return rule.apply(rest, ruleLookup);
+        }, {
+          pass: true,
+          rest: [input]
+        });
+      }).filter(({ pass }) => pass);
+    })
+
+    if (results.some((r) => !r.rest.length)) return { pass: true, rest: [] };
+    return { pass: !!results.length, rest: [...new Set(results.flatMap((r) => r.rest))] };
+  }
+}
+
 export class Question extends QuestionBase {
   constructor() {
-    super(2020, 19);
+    super(2020, 19, 208, 316);
+
+    this.exampleInput({ part1: 2 });
+    this.exampleInput({ part1: 3, part2: 12 });
   }
 
   get parser() {
-    return Parsers.ONE_NUMBER_PER_LINE;
+    return Parsers.MULTIPLE;
   }
 
-  part1(input) {
-    return input.length;
+  get parsers() {
+    return {
+      'rules': Parsers.PROPERTY_LIST,
+      'input': Parsers.PARSER
+    };
   }
 
-  part2(input) {
-    return input.reduce((a, b) => a + b, 0);
+  parseValue(value, key) {
+    return new Rule(key, value);
+  }
+
+  parserGroup(line) {
+    return /:/.test(line) ? 'rules' : 'input';
+  }
+
+  countPassing({ rules, input }) {
+    return input.filter((i) => {
+      const { pass, rest } = rules[0].apply([i], rules);
+      return pass && rest.length === 0;
+    }).length;
+  }
+
+  part1({ rules, input }) {
+    return this.countPassing({ rules, input });
+  }
+
+  part2({ rules, input }) {
+    rules[8] = new Rule(8, '42 | 42 8');
+    rules[11] = new Rule(11, '42 31 | 42 11 31');
+
+    return this.countPassing({ rules, input });
   }
 }
