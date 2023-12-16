ffff8f81a8e63d4e97f2a717512440eab2deb7c4
Hugh Greenish <hugh.greenish@mmc.com>
2023-12-16 09:19:00 +0000

    better

--- answers/2023/16.js
+++ answers/2023/16.js
@@ -12,65 +12,51 @@ export class Question extends QuestionBase {
     return Parsers.GRID;
   }
 
-  energisedSquares(from, grid, adjacencyMap, width) {
-    let square = from.x;
-    let direction = from.y;
-    const squares = [from];
-    const additionalStarts = [];
-    while (true) {
-      switch (grid[square]) {
-        case '\\':
-          direction = Math.sign(direction) * (Math.abs(direction) === 1 ? width : 1);
-          break;
-        case '/':
-          direction = -Math.sign(direction) * (Math.abs(direction) === 1 ? width : 1);
-          break;
-        case '-':
-          if (Math.abs(direction) === width) additionalStarts.push(new Vector(square, -1));
-          direction = Math.abs(direction) === 1 ? direction : 1;
-          break;
-        case '|':
-          if (Math.abs(direction) === 1) additionalStarts.push(new Vector(square, -width));
-          direction = Math.abs(direction) === width ? direction : width;
-          break;
-      }
-      let next = new Vector(square + direction, direction);
-      if (!squares.some((s) => s.equals(next)) && adjacencyMap[square].includes(square + direction)) {
-        squares.push(next);
-        square += direction;
-      } else if (additionalStarts.length) {
-        next = additionalStarts.pop();
-        while (additionalStarts.length && squares.some((s) => s.equals(next))) next = additionalStarts.pop();
-        if (!squares.some((s) => s.equals(next))) {
-          squares.push(next);
-          square = next.x;
-          direction = next.y;
-        } else {
-          return squares;
-        }
-      } else {
-        return squares;
-      }
+  getNewDirections(cell, direction, width) {
+    switch (cell) {
+      case '\\':
+        return [Math.sign(direction) * (Math.abs(direction) === 1 ? width : 1)];
+      case '/':
+        return [-Math.sign(direction) * (Math.abs(direction) === 1 ? width : 1)];
+      case '-':
+        return Math.abs(direction) === 1 ? [direction] : [1, -1];
+      case '|':
+        return Math.abs(direction) === 1 ? [width, -width] : [direction];
+      default:
+        return [direction];
     }
   }
 
-  countEnergisedSquares(start, grid, adjacencyMap, width) {
-    const squares = this.energisedSquares(start, grid, adjacencyMap, width);
-    return [...new Set(squares.map(({ x }) => x))].length;
+  energisedSquares(from, grid, adjacencyMap, width) {
+    const stack = [from];
+    const seen = {};
+    while (stack.length) {
+      const { square, direction } = stack.pop();
+      if (seen[square]?.includes(direction)) continue;
+      seen[square] ||= [];
+      seen[square].push(direction);
+
+      const nextSquares = this.getNewDirections(grid[square], direction, width)
+        .filter((d) => adjacencyMap[square].includes(square + d))
+        .map((d) => ({ square: square + d, direction: d }));
+      stack.push(...nextSquares);
+    }
+
+    return Object.keys(seen).length;
   }
 
   part1({ grid, width, adjacencyMap }) {
-    return this.countEnergisedSquares(new Vector(0, 1), grid, adjacencyMap, width);
+    return this.energisedSquares({ square: 0, direction: 1 }, grid, adjacencyMap, width);
   }
 
   part2({ grid, width, height, adjacencyMap }) {
     const starts = [
-      ...Array.from({ length: width }, (_, ix) => new Vector(ix, width)),
-      ...Array.from({ length: width }, (_, ix) => new Vector(grid.length - 1 - ix, -width)),
-      ...Array.from({ length: height }, (_, ix) => new Vector(ix * width, 1)),
-      ...Array.from({ length: height }, (_, ix) => new Vector((ix + 1) * width - 1, -1)),
+      ...Array.from({ length: width }, (_, ix) => ({ square: ix, direction: width })),
+      ...Array.from({ length: width }, (_, ix) => ({ square: grid.length - 1 - ix, direction: -width })),
+      ...Array.from({ length: height }, (_, ix) => ({ square: ix * width, direction: 1 })),
+      ...Array.from({ length: height }, (_, ix) => ({ square: (ix + 1) * width - 1, direction: -1 })),
     ];
-    const ends = starts.map((s) => this.countEnergisedSquares(s, grid, adjacencyMap, width));
+    const ends = starts.map((s) => this.energisedSquares(s, grid, adjacencyMap, width));
     return ends.reduce((a, b) => Math.max(a, b));
   }
 }
